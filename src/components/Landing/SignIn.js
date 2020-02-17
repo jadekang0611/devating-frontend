d6d89563f3a06e996780cbfc316f4c311b5c7467
jadekang <jkang.se@gmail.com>
2020-02-16 22:41:37 -0800

    sign in, api sign in get method implemented and rerouting to dashboard

--- src/components/Landing/SignIn.js
+++ src/components/Landing/SignIn.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { Link, Switch, Route, Router, Redirect } from 'react-router-dom';
+import { browserHistory } from 'react-router';
+const axios = require('axios');
 
-function SignIn() {
+function SignIn(props) {
   const [inputEmail, setInputEmail] = useState('');
   const [inputPassword, setInputPassword] = useState('');
 
@@ -17,6 +19,27 @@ function SignIn() {
     setInputPassword(e.target.value);
     console.log(e.target.value);
   }
+
+  function handleSignIn(e) {
+    e.preventDefault();
+
+    console.log('Email' + inputEmail);
+    console.log('Password' + inputPassword);
+
+    axios.get(url + '/' + inputEmail + '/' + inputPassword).then(res => {
+      let user = res.data;
+      if (user.length !== 0) {
+        props.history.push({
+          pathname: '/dashboard',
+          state: {user: user} 
+        });
+      } else {
+        alert('SOMETHING WENT WRONG');
+      }
+    });
+  }
+
+  const url = 'http://localhost:7000/signin';
 
   return (
     <div>
@@ -35,7 +58,7 @@ function SignIn() {
           onChange={handlePasswordChange}
         ></input>
 
-        <button type="submit">
+        <button type="submit" onClick={handleSignIn}>
           Sign In
         </button>
       </form>
