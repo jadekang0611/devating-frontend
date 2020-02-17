3409da5b453950952a3ee396c69db5fc868201b5
Abdul James <abduljames@Abduls-iMac.local>
2020-02-17 00:38:55 -0500

    added pics and some info on founders

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,6 +21,49 @@ function About() {
         proident deserunt incididunt nostrud velit consequat non est deserunt.
         Ullamco eu minim qui minim velit.
       </section>
+      <h2>Founders</h2>
+      <h3>Jade Kang</h3>
+      <p>Software Engineer</p>
+      <p>jkang.se@gmail.com</p>
+      <p>linkedInJade..</p>
+      <p>jadeGitHub</p>
+
+      <div>
+        <img
+          src="https://files.slack.com/files-pri/T0351JZQ0-FU2P2MAER/1581884468137.jpg"
+          alt="Jade Kang"
+        />
+        <h3>Daisy Yau</h3>
+        <p>Software Engineer</p>
+        <p>daisy125@gmail.com</p>
+        <p>linkedInDaisy..</p>
+        <p>DaisyGitHub</p>
+        <img
+          src="https://ca.slack-edge.com/T0351JZQ0-UQNFCFLPK-9eaf00fa8ca3-512"
+          alt="Daisy Yau"
+        />
+        <h3>Sage Kearney</h3>
+        <p>Software Engineer</p>
+        <p>sagekearney@gmail.com</p>
+        <p>
+          linkedInSage..
+          <p>SageGitHub</p>
+        </p>
+        <img
+          src="https://files.slack.com/files-pri/T0351JZQ0-FU2QJPXK2/small.jpg"
+          alt="Sage Kearney"
+        />
+        <h3>Abdul James</h3>
+        <p>Software Engineer</p>
+        <p>AbdulJames2020@gmail.com</p>
+        <p>https://www.linkedin.com/in/abdul-s-james-031975/</p>
+        <p>https://github.com/sewatu999/</p>
+
+        <img
+          src="https://ca.slack-edge.com/T0351JZQ0-UQNFCEPQR-fddec0ff9d8e-512"
+          alt="Abdul James"
+        />
+      </div>
     </div>
   );
 }
