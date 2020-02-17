c591d594f4a605d63c0587fd129349937f457c2b
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-02-17 11:16:51 -0500

    simplified nav compnent to display about, see your saved matches, & devating gets your romantic programmers. added create new account & sign in buttons to page.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,15 +8,15 @@ function Nav() {
         <li>
           <Link to="about">About</Link>
         </li>
+        {/* <li>
+            <Link to="/signin">Sign In</Link>
+          </li> */}
+        {/* <li> */}
+        {/* {' '} <--- was this supposed to be something? or just an accident? */}
+        {/* <Link to="/createAccount">Create a new account</Link>
+          </li> */}
         <li>
-          <Link to="/signin">Sign In</Link>
-        </li>
-        <li>
-          {' '}
-          <Link to="/createAccount">Create a new account</Link>
-        </li>
-        <li>
-          <Link to="/dashboard">See Your Matches</Link>
+          <Link to="/dashboard">See Your Saved Matches</Link>
         </li>
         <li>
           <Link to="/">Devating Gets Your Romantic Programmers</Link>
