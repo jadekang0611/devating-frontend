1b20ba83b2dbab49a2b046aa484b3002cbb086b0
daisyy125 <daisy125@gmail.com>
2020-02-17 11:46:23 -0500

    add routing to question two component

--- src/components/Question/QuestOne.js
+++ src/components/Question/QuestOne.js
@@ -5,7 +5,7 @@ import { Link, Switch, Route, Redirect } from 'react-router-dom';
 
 function QuestOne() {
     const [userAge, setUserAge] = useState('');
-    const [gender, setUserGender] = useState('');
+    const [Usergender, setUserGender] = useState('');
     // const [submit, setSubmit] = useState('');
 
     function handleAgeChange(e) {
@@ -24,15 +24,15 @@ function QuestOne() {
         <div>
             <form onClick={handleSubmit}>
                 <input type="text" placeholder="Age" id="age" value={userAge} onChange={handleAgeChange} />
-                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623751-dbad0800-5113-11ea-81e9-462ee661a1c0.png" alt="submit" value={gender}/>
-                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623934-7f96b380-5114-11ea-84a9-0fe8daf9122b.png" alt="submit" value={gender}/>
+                <p>Your Gender</p>
+                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623751-dbad0800-5113-11ea-81e9-462ee661a1c0.png" alt="submit" value={Usergender}/>
+                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623934-7f96b380-5114-11ea-84a9-0fe8daf9122b.png" alt="submit" value={Usergender}/>
                 <Link to='/2'>
-                <button type="submit">=> Next</button>
+                <button type="submit"> 1 / 4 => Next</button>
                 </Link>
                 <Switch>
                     <Route path='/2' exact component={QuestTwo}/>
                 </Switch>
-                <p>1 / 4</p>
             </form>
         </div>
     );
