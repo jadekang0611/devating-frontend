fe55a32fdeaa903fa5ce879c9f1ad6e873dadff4
daisyy125 <daisy125@gmail.com>
2020-02-17 15:45:07 -0500

    update loading css file

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './loading.css';
 
 function Loading() {
   return (
-    <div>
+    <div className="loading">
       <h2>Refactoring your matches</h2>
-      <img
+      <img className="loadingIcon"
         src='https://user-images.githubusercontent.com/58707118/74624812-f2555e00-5117-11ea-9825-7ca3a6623c88.png'
         alt='refactoring'></img>
     </div>
