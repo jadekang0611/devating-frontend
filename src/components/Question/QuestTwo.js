062d2b3b84efacb9c9876add0aa6ddd06a3553c2
daisyy125 <daisy125@gmail.com>
2020-02-17 11:46:23 -0500

    add routing to question two component

--- src/components/Question/QuestTwo.js
+++ src/components/Question/QuestTwo.js
@@ -1,29 +1,41 @@
 import React from 'react';
+import QuestThree from './QuestThree';
+import { useState } from 'react';
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
 
 function QuestTwo() {
+  const [WantGender, setWantGender] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
+    setWantGender(e.target.value);
     console.log(e.target.value);
   }
 
   return (
     <div>
       <form>
+        <p>You're Interested In:</p>
         <input
           type='image'
           src='https://user-images.githubusercontent.com/58707118/74623751-dbad0800-5113-11ea-81e9-462ee661a1c0.png'
           alt='submit'
+          value={WantGender}
         />
         <input
           type='image'
           src='https://user-images.githubusercontent.com/58707118/74623934-7f96b380-5114-11ea-84a9-0fe8daf9122b.png'
           alt='submit'
+          value={WantGender}
         />
-        <button type='submit' onClick={handleSubmit}>
-          => Next
-        </button>
-        <p>2 / 4</p>
+        <Link to='/3'>
+          <button type='submit' onClick={handleSubmit}>
+            2 / 4 => Next
+          </button>
+        </Link>
+        <Switch>
+          <Route path='/3' exact component={QuestThree} />
+        </Switch>
       </form>
     </div>
   );
