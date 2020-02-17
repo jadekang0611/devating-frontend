25a6728e5bcb2fc8cc619019b7cbf1c2c8605a9e
daisyy125 <daisy125@gmail.com>
2020-02-17 00:17:50 -0500

    update question components and loading component

--- src/components/Question/QuestFour.js
+++ src/components/Question/QuestFour.js
@@ -1,9 +1,47 @@
 import React from 'react';
 
 function QuestFour() {
-    return (
-        <h3>This is question four component: will be called under Question component</h3>
-    );
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
+
+  return (
+    <div>
+      <h3>Some of your favorite coding languages</h3>
+      <form>
+        <input
+          type='image'
+          src='https://user-images.githubusercontent.com/58707118/74624172-735f2600-5115-11ea-9819-8bf512bbbf1e.png'
+          alt='submit'
+        />
+        <input
+          type='image'
+          src='https://user-images.githubusercontent.com/58707118/74624173-73f7bc80-5115-11ea-8b3b-54fd87d84fd7.png'
+          alt='submit'
+        />
+        <input
+          type='image'
+          src='https://user-images.githubusercontent.com/58707118/74624174-73f7bc80-5115-11ea-90b9-8f723e1599e3.png'
+          alt='submit'
+        />
+        <input
+          type='image'
+          src='https://user-images.githubusercontent.com/58707118/74624175-73f7bc80-5115-11ea-9a53-f21404677e70.png'
+          alt='submit'
+        />
+        <input
+          type='image'
+          src='https://user-images.githubusercontent.com/58707118/74624176-73f7bc80-5115-11ea-8f4c-f228ff3e71d5.png'
+          alt='submit'
+        />
+        <button type='submit' onClick={handleSubmit}>
+          => Next
+        </button>
+        <p>4 / 4</p>
+      </form>
+    </div>
+  );
 }
 
 export default QuestFour;
--- src/components/Question/QuestTwo.js
+++ src/components/Question/QuestTwo.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 function QuestTwo() {
-    return (
-        <h3>This is question two component: will be called under Question component</h3>
-    );
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          type='image'
+          src='https://user-images.githubusercontent.com/58707118/74623751-dbad0800-5113-11ea-81e9-462ee661a1c0.png'
+          alt='submit'
+        />
+        <input
+          type='image'
+          src='https://user-images.githubusercontent.com/58707118/74623934-7f96b380-5114-11ea-84a9-0fe8daf9122b.png'
+          alt='submit'
+        />
+        <button type='submit' onClick={handleSubmit}>
+          => Next
+        </button>
+        <p>2 / 4</p>
+      </form>
+    </div>
+  );
 }
 
 export default QuestTwo;
