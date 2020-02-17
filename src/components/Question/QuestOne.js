f463b726a188cf4e7031000da45309d11f30f612
daisyy125 <daisy125@gmail.com>
2020-02-17 00:17:50 -0500

    update question components and loading component

--- src/components/Question/QuestOne.js
+++ src/components/Question/QuestOne.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 
 function QuestOne() {
+    const [userAge, setUserAge] = useState('');
+    const [submit, setSubmit] = useState('');
+
+    function handleAgeChange(e) {
+        e.preventDefault();
+        setUserAge(e.target.value);
+        console.log(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setSubmit(e.target.value);
+        console.log(e.target.value);
+    }
+
     return (
-        <h3>This is question one component: will be called under Question component</h3>
+        <div>
+            <form>
+                <input type="text" placeholder="Age" id="age" value={userAge} onChange={handleAgeChange} />
+                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623751-dbad0800-5113-11ea-81e9-462ee661a1c0.png" alt="submit" />
+                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623934-7f96b380-5114-11ea-84a9-0fe8daf9122b.png" alt="submit" />
+                <button type="submit" onClick={handleSubmit}>=> Next</button>
+                <p>1 / 4</p>
+            </form>
+        </div>
     );
 }
 
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -3,16 +3,17 @@ import QuestOne from './QuestOne';
 import QuestTwo from './QuestTwo';
 import QuestThree from './QuestThree';
 import QuestFour from './QuestFour';
+import './question.css';
 
 
 function Question() {
     return (
         <div>
-        <h3>This is question component: to hold 4 questions component</h3>
-        <QuestOne />
-        <QuestTwo />
-        <QuestThree />
-        <QuestFour />
+            <h2>Create New</h2>
+            <QuestOne />
+            <QuestTwo />
+            <QuestThree />
+            <QuestFour />
         </div>
     );
 }
