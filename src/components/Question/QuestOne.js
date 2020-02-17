fbd5e25f32cd5bd9377ac6f3e9313e6f45366e8e
daisyy125 <daisy125@gmail.com>
2020-02-17 10:30:51 -0500

    update with routing from question 1 to question 2

--- src/components/Question/QuestOne.js
+++ src/components/Question/QuestOne.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import QuestTwo from './QuestTwo'
 import { useState } from 'react';
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
 
 function QuestOne() {
     const [userAge, setUserAge] = useState('');
-    const [submit, setSubmit] = useState('');
+    const [gender, setUserGender] = useState('');
+    // const [submit, setSubmit] = useState('');
 
     function handleAgeChange(e) {
         e.preventDefault();
@@ -13,17 +16,22 @@ function QuestOne() {
 
     function handleSubmit(e) {
         e.preventDefault();
-        setSubmit(e.target.value);
+        setUserGender(e.target.value);
         console.log(e.target.value);
     }
 
     return (
         <div>
-            <form>
+            <form onClick={handleSubmit}>
                 <input type="text" placeholder="Age" id="age" value={userAge} onChange={handleAgeChange} />
-                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623751-dbad0800-5113-11ea-81e9-462ee661a1c0.png" alt="submit" />
-                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623934-7f96b380-5114-11ea-84a9-0fe8daf9122b.png" alt="submit" />
-                <button type="submit" onClick={handleSubmit}>=> Next</button>
+                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623751-dbad0800-5113-11ea-81e9-462ee661a1c0.png" alt="submit" value={gender}/>
+                <input type="image" src="https://user-images.githubusercontent.com/58707118/74623934-7f96b380-5114-11ea-84a9-0fe8daf9122b.png" alt="submit" value={gender}/>
+                <Link to='/2'>
+                <button type="submit">=> Next</button>
+                </Link>
+                <Switch>
+                    <Route path='/2' exact component={QuestTwo}/>
+                </Switch>
                 <p>1 / 4</p>
             </form>
         </div>
