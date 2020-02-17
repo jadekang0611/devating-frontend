4ee2d898c154d97e7d003b1c7faa7214747c1e3f
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-02-17 18:31:12 -0500

    single matchcard displaying, next button is working

--- src/components/MatchCard/MatchCard.js
+++ src/components/MatchCard/MatchCard.js
@@ -21,35 +21,47 @@ const [imgs, setImgs] = useState([
         id: 4
     }
 ]);
+const [matchNum, setMatchNum] = useState(0);
+
+const onClick = () => {
+  if(matchNum < imgs.length - 1){
+    setMatchNum( matchNum + 1);
+  } else {
+    setMatchNum(0);
+  }
+}
 
 // set all match cards to display none, except for first match
-const cardStyle = {
-    display: 'block'
-};
+// const cardStyle = {
+//     display: 'block'
+// };
 
-const firstMatch = {
-    display:'block'
-}
+// const firstMatch = {
+//     display:'block'
+// }
 // ^maybe I can set image[0] style={firstMatch}
 // change display none to display block/flex when next button is clicked
-let currentMatch = 0;
+// let currentMatch = 0;
 
-function filterMatches(image){
-    console.log(image.id);
-    const previousMatch = currentMatch - 1;
-    const current = imgs[currentMatch];
-    const previous = imgs[previousMatch];
+// function filterMatches(image){
+//     console.log(image.id);
+//     const previousMatch = currentMatch - 1;
+//     const current = imgs[currentMatch];
+//     const previous = imgs[previousMatch];
 
 
-}
+// }
 
 // once next button is clicked, previous match display = none
 
     return (
       <div className="card-container">
-        {imgs.map(image => {
-          let first = image[0];
+        {/* {imgs.map(image => {
+          // let first = image[0];
           return (
+            // if there are matches, display them
+
+            // esle display "you currently have no matches :( "
             <div key={image.id} style={cardStyle} >
               <img src={image.img}></img>
               <div className="match-info">
@@ -69,7 +81,25 @@ function filterMatches(image){
               </button>
             </div>
           );
-        })}
+        })} */}
+        <div key={imgs[matchNum].id}>
+          <img src={imgs[matchNum].img} alt="flower picture"></img>
+          <div className="match-info">
+            <h3 className="match-name-age">Name, age</h3>
+            <p>Both you and 'match name' like these languages..</p>
+          </div>
+          <div className="icon-container">
+            <div className="code-lang-icon">icon</div>
+            <div className="code-lang-icon">icon</div>
+            <div className="code-lang-icon">icon</div>
+          </div>
+          <button onClick={onClick} className="match-button">
+            keep
+          </button>
+          <button onClick={onClick} className="match-button">
+            next
+          </button>
+        </div>
       </div>
     );
 }
