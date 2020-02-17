c64d5c5fe292b7d8b30219561dc8cd486218c8fe
daisyy125 <daisy125@gmail.com>
2020-02-17 00:27:33 -0500

    added loading component with icon

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function Loading() {
-    return (
-        <h3>This is loading component: logo and refactoring results page</h3>
-    );
+  return (
+    <div>
+      <h2>Refactoring your matches</h2>
+      <img
+        src='https://user-images.githubusercontent.com/58707118/74624812-f2555e00-5117-11ea-9825-7ca3a6623c88.png'
+        alt='refactoring'></img>
+    </div>
+  );
 }
 
 export default Loading;
