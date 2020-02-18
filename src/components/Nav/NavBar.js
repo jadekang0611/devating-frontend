7fea88ac5830b12ac3af47ffe855396365a18517
daisyy125 <daisy125@gmail.com>
2020-02-18 16:35:35 -0500

    update styling for nav bar

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -7,24 +7,14 @@ import logo from '../../devating-logo.png';
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="justify-content-end ">
+    <Navbar collapseOnSelect expand="lg">
+      <h1 className="navTitle">Devating</h1>
       <Navbar.Toggle
         aria-controls="responsive-navbar-nav"
         className="navbar-toggler-right"
       />
 
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Link to="/">
-          <Navbar.Brand>
-            <img
-              alt=""
-              src={logo}
-              width="30%"
-              height="auto"
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-        </Link>
         <Nav className="ml-auto">
           <Nav.Link className="nav-item">
             <Link to="about" className="items">
