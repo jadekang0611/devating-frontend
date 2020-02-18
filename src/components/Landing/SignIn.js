5a08ad566dacb25ab302a4e73f1cff1f9533426e
jadekang <jkang.se@gmail.com>
2020-02-17 17:57:41 -0800

    style updated

--- src/components/Landing/SignIn.js
+++ src/components/Landing/SignIn.js
@@ -56,9 +56,6 @@ function SignIn(props) {
             placeholder="Enter email"
             onChange={handleEmailChange}
           />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
         <Form.Group className="input-field" controlID="formGroupEmail">
           <Form.Control
