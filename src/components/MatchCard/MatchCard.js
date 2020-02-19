ce221b6f5a992b5d57151a28e62c3259d298c9bc
Abdul James <abduljames@Abduls-iMac.local>
2020-02-19 01:16:41 -0500

    got data to show on results components

--- src/components/MatchCard/MatchCard.js
+++ src/components/MatchCard/MatchCard.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './MatchCard.css';
 import { useState, useEffect } from 'react';
 import cooking from '../../icons/cooking.svg';
+import Results from '../Results/Results';
 
 const axios = require('axios');
 
 function MatchCard(props) {
   const url = 'http://localhost:7000/match/';
   const email = 'sharline@email.com';
-  const dynamicEmail = props.location.state.email;
+  // const dynamicEmail = props.location.state.email;
   const matches = props.matches;
 
   const [match, setMatch] = useState([]);
@@ -19,66 +20,70 @@ function MatchCard(props) {
 
   console.log(match);
 
+  // const sendMatches = match => {
+  //   props.getMatchData(match);
+  // };
   //  const data = match;
 
-  const [imgs, setImgs] = useState([
-    {
-      img:
-        'https://images.pexels.com/photos/1253718/pexels-photo-1253718.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      id: 1
-    },
-    {
-      img:
-        'https://images.pexels.com/photos/931179/pexels-photo-931179.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      id: 2
-    },
-    {
-      img:
-        'https://images.pexels.com/photos/1381679/pexels-photo-1381679.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      id: 3
-    },
-    {
-      img:
-        'https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      id: 4
-    }
-  ]);
+  // const [imgs, setImgs] = useState([
+  //   {
+  //     img:
+  //       'https://images.pexels.com/photos/1253718/pexels-photo-1253718.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  //     id: 1
+  //   },
+  //   {
+  //     img:
+  //       'https://images.pexels.com/photos/931179/pexels-photo-931179.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  //     id: 2
+  //   },
+  //   {
+  //     img:
+  //       'https://images.pexels.com/photos/1381679/pexels-photo-1381679.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  //     id: 3
+  //   },
+  //   {
+  //     img:
+  //       'https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  //     id: 4
+  //   }
+  // ]);
   const [matchNum, setMatchNum] = useState(0);
 
   const onClick = () => {
-    if (matchNum < imgs.length - 1) {
-      setMatchNum(matchNum + 1);
-    } else {
-      setMatchNum(0);
-    }
+    // if (matchNum < imgs.length - 1) {
+    //   setMatchNum(matchNum + 1);
+    // } else {
+    //   setMatchNum(0);
+    // }
   };
 
   return (
     <div className="card-container">
-      <div key={imgs[matchNum].id}>
-        <img src={imgs[matchNum].img} alt="flower picture"></img>
-        <div className="match-info">
-          <h3 className="match-name-age">Name, age</h3>
-          <p>Both you and 'match name' like these languages..</p>
-        </div>
-        <div className="icon-container">
-          <div className="code-lang-icon">
-            <img src={cooking}></img>
-          </div>
-          <div className="code-lang-icon">
-            <img src={cooking}></img>
-          </div>
-          <div className="code-lang-icon">
-            <img src={cooking}></img>
-          </div>
-        </div>
-        <button onClick={onClick} className="match-button">
-          keep
-        </button>
-        <button onClick={onClick} className="match-button">
-          next
-        </button>
+      <Results matches={match} />
+      {/* <div key={imgs[matchNum].id}>
+        <img src={imgs[matchNum].img} alt="flower picture"></img> */}
+      <div className="match-info">
+        <h3 className="match-name-age">Name, age</h3>
+        <p>Both you and 'match name' like these languages..</p>
       </div>
+      <div className="icon-container">
+        <div className="code-lang-icon">
+          <img src={cooking}></img>
+        </div>
+        <div className="code-lang-icon">
+          <img src={cooking}></img>
+        </div>
+        <div className="code-lang-icon">
+          <img src={cooking}></img>
+        </div>
+      </div>
+      <button onClick={onClick} className="match-button">
+        keep
+      </button>
+      <button onClick={onClick} className="match-button">
+        next
+      </button>
+      {/* </div> */}
     </div>
   );
 }
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -2,18 +2,29 @@ import React from 'react';
 import MatchCard from '../MatchCard/MatchCard';
 
 function Results(props) {
-    return (
-      <div>
-        <h3>Take a chance on..</h3>
-        {MatchCard.map(match => {
-          return (
-            <div>
-              <MatchCard name={match.name} email={match.email} age={match.age} coding={match.favoriteCoding} />
-            </div>
-          );
-        })}
-      </div>
-    );
+  // let matchArr = [];
+  console.log(props.matches.name);
+  // const getMatchData = matches => {
+  //   matchArr = matches;
+  //   console.log(matchArr);
+  //   console.log(props);
+  // };
+  return (
+    <div>
+      <h3>Take a chance on..</h3>
+      {props.matches.map(match => {
+        return (
+          <div>
+            <p>name={match.name}</p>
+            <p>email={match.email}</p>
+            <p>age={match.age}</p>
+            <p>coding={match.favoriteCoding}</p>
+            {/* // getMatchData={getMatchData} */}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Results;
