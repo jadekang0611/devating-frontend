af93759636439352b3ebe853dcf5c696c2245ebf
jadekang <jkang.se@gmail.com>
2020-02-19 11:27:18 -0800

    added put method for updating my keep matching array

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -24,6 +24,7 @@ const codingImages = [
 ];
 
 function Results(props) {
+  const [keepArray, setKeepArray] = useState([]);
   const url = 'http://localhost:7000/match/';
   const email = 'jadekang@gmail.com';
   // const email = props.location.state.email;
@@ -35,8 +36,6 @@ function Results(props) {
     axios.get(url + email).then(res => setMatch(res.data));
   }, []);
 
-
-
   //console.log(arr[0]);
 
   const onClick = () => {
@@ -47,80 +46,93 @@ function Results(props) {
     }
   };
 
+  const onKeep = () => {
+    let keepObj = {
+      name: matches[matchNum].name,
+      image: matches[matchNum].image
+    };
+    console.log('keepArray: ' + JSON.stringify(keepArray));
+    keepArray.push(keepObj);
+    console.log('keepArray after push: ' + JSON.stringify(keepArray));
+    setKeepArray(keepArray);
+    console.log('keepArray after setKeepArray: ' + JSON.stringify(keepArray));
+    axios
+      .put(url + 'update/' + email, { keep: keepArray })
+      .then(() => onClick());
+  };
+    
 
-
-if (matches.length > 0){
-
-  if (matchNum === matches.length-1){
-return (
-  <div>
-  <h3>Take a chance on..</h3>
-  <div className="card-container">
-    <div>
-       <img src={matches[matchNum].image} alt="flower"></img>  
-      <div className="match-info">
-        <h3 className="match-name-age">{matches[matchNum].name}, {matches[matchNum].age}</h3>
-        <p>Both you and {matches[matchNum].name} like these languages..</p>
-      </div>
-      <div className="icon-container">
-      {matches[matchNum].favoriteCoding.map(code => {
-            return (
-              <div className="code-lang-icon">
-                <img src={codingImages[code.id]}></img>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <button onClick={onClick} className="match-button">
-        keep
-      </button>
-    </div>
-  </div>
-);
-  }
-  else{
-  return (
-    <div>
-      <h3>Take a chance on..</h3>
-      <div className="card-container">
+  if (matches.length > 0) {
+    if (matchNum === matches.length - 1) {
+      return (
         <div>
-           <img src={matches[matchNum].image} alt="flower"></img>  
-          <div className="match-info">
-            <h3 className="match-name-age">{matches[matchNum].name}, {matches[matchNum].age}</h3>
-            <p>Both you and {matches[matchNum].name} like these languages..</p>
-          </div>
-          <div className="icon-container">
-          {matches[matchNum].favoriteCoding.map(code => {
-            return (
-              <div className="code-lang-icon">
-                <img src={codingImages[code.id]}></img>
+          <h3>Take a chance on..</h3>
+          <div className="card-container">
+            <div>
+              <img src={matches[matchNum].image} alt="flower"></img>
+              <div className="match-info">
+                <h3 className="match-name-age">
+                  {matches[matchNum].name}, {matches[matchNum].age}
+                </h3>
+                <p>
+                  Both you and {matches[matchNum].name} like these languages..
+                </p>
               </div>
-            );
-          })}
+              <div className="icon-container">
+                {matches[matchNum].favoriteCoding.map(code => {
+                  return (
+                    <div className="code-lang-icon">
+                      <img src={codingImages[code.id]}></img>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
+            <button
+              onClick={onKeep(matches[matchNum])}
+              className="match-button"
+            >
+              keep
+            </button>
           </div>
-          <button onClick={onClick} className="match-button">
-            keep
-          </button>
-          <button onClick={onClick} className="match-button">
-            next
-          </button>
         </div>
-      </div>
-  ); 
-          }
-
+      );
+    } else {
+      return (
+        <div>
+          <h3>Take a chance on..</h3>
+          <div className="card-container">
+            <div>
+              <img src={matches[matchNum].image} alt="flower"></img>
+              <div className="match-info">
+                <h3 className="match-name-age">
+                  {matches[matchNum].name}, {matches[matchNum].age}
+                </h3>
+                <p>
+                  Both you and {matches[matchNum].name} like these languages..
+                </p>
+              </div>
+              <div className="icon-container">
+                {matches[matchNum].favoriteCoding.map(code => {
+                  return (
+                    <div className="code-lang-icon">
+                      <img src={codingImages[code.id]}></img>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <button onClick={onKeep} className="match-button">
+              keep
+            </button>
+            <button onClick={onClick} className="match-button">
+              next
+            </button>
+          </div>
+        </div>
+      );
+    }
+  } else return <div></div>;
 }
-else
-return (
-  <div></div>
-)
-
-
-
-}
-
-
 
 export default Results;
