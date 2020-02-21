aff7daf77f6a57a4ebae6931e916e421b97187c2
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-02-20 22:47:30 -0500

    changes in loading page before switching to dev branch

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import './loading.css';
+import { Redirect } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-function Loading() {
+
+function Loading(props) {
+  const [redirect, setRedirect] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setRedirect(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  
   return (
     <div className="loading">
-      <h2>Refactoring your matches</h2>
-      <img className="loadingIcon"
-        src='https://user-images.githubusercontent.com/58707118/74624812-f2555e00-5117-11ea-9825-7ca3a6623c88.png'
-        alt='refactoring'></img>
+      {redirect && <Redirect to="/results" />}
+      {!redirect && (
+        <div>
+          <h2>Refactoring your matches</h2>
+          <img
+            className="loadingIcon"
+            src="https://user-images.githubusercontent.com/58707118/74624812-f2555e00-5117-11ea-9825-7ca3a6623c88.png"
+            alt="refactoring"
+          ></img>
+        </div>
+      )}
     </div>
   );
-
 }
-
 export default Loading;
